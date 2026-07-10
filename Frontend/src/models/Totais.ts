ef8bdf07b99626dd730ddbd76d678e1666034d59
BrunoWitt <brunowittn@gmail.com>
2026-07-10 16:45:52 -0300

    feat: modelos do banco de dados para o front

---
+++ Frontend/src/models/Totais.ts
@@ -0,0 +1,24 @@
+export interface TotalPessoa {
+
+    pessoaId: number;
+
+    nome: string;
+
+    totalReceitas: number;
+
+    totalDespesas: number;
+
+    saldo: number;
+
+}
+
+
+export interface TotalGeral {
+
+    totalReceitas: number;
+
+    totalDespesas: number;
+
+    saldo: number;
+
+}
---
+++ Frontend/src/models/Transacao.ts
@@ -0,0 +1,17 @@
+export type TipoTransacao =
+    "Receita" | "Despesa";
+
+
+export interface Transacao {
+
+    id:number;
+
+    descricao:string;
+
+    valor:number;
+
+    tipo:TipoTransacao;
+
+    pessoaId:number;
+
+}
